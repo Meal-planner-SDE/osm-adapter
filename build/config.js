542fdb5d8c6e508821f61a4fdb48d9ba5ee623e0
Gabriele Masina <gabriele.masina@virgilio.it>
2021-01-24 21:06:48 +0100

    Remove build

--- build/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    OVERPASS_API_ENDPOINT: 'https://z.overpass-api.de/api/interpreter/',
-    NOMINATIM_API_ENDPOINT: 'https://nominatim.openstreetmap.org/',
-};
